e213559ed26f7ca1b92cbf61dd8d3056538ddab4
kbrajeshwaran <kbrajeshwaran@gmail.com>
2022-11-30 05:09:54 +0400

    docker file created

---
+++ acr.sh
@@ -0,0 +1,8 @@
+ACR_RESOURCE_GROUP_NAME="rg-kbbrajesh-azdevops-acrimage"
+ACR_NAME="acrkbbazdevops2731"
+
+# Create a resource group to store container registry
+az group create --name $ACR_RESOURCE_GROUP_NAME --location uksouth
+
+# Create a container registry that will be where you deploy your image to
+az acr create --resource-group $ACR_RESOURCE_GROUP_NAME --name $ACR_NAME --sku Basic
